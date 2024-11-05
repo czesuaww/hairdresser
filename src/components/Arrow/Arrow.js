12256ac2c7949af1a024fe4aa2bd9e2044978767
Grzegorz_Szwed <greg152295@gmail.com>
2024-11-05 19:25:23 +0100

    New component arrow, added to header and added function to scroll view down to specific ID

---
+++ src/components/Arrow/Arrow.js
@@ -0,0 +1,19 @@
+import style from './Arrow.module.css';
+
+const Arrow = () => {
+
+    const scrollDown = () => {
+        const aboutMeSection = document.getElementById('#aboutMeSection')
+        if (aboutMeSection) aboutMeSection.scrollIntoView();
+    }
+
+    return (
+        <div className={style.container}>
+            <div onClick={scrollDown}>
+                <div className={style.arrow}></div>
+            </div>
+        </div>
+    );
+}
+
+export default Arrow;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import style from './Header.module.css';
+import Arrow from '../Arrow/Arrow.js';
 
 const Header = () => {
     return (
         <div className={style.header}>
             <header className={style.firstPic}></header>
             <p className={style.title}>Chcesz poznać więcej moich prac?</p>
+            <Arrow />
         </div>
     )
 }
