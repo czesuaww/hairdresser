b79d9ca525cd826dfc406741ceccdbfdec975dcb
Grzegorz_Szwed <greg152295@gmail.com>
2024-11-05 19:33:23 +0100

    Added smooth behavior and added component to main app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/Header/Header.js'
+import Header from './components/Header/Header.js';
+import About from './components/About/About.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
--- src/components/Arrow/Arrow.js
+++ src/components/Arrow/Arrow.js
@@ -3,8 +3,8 @@ import style from './Arrow.module.css';
 const Arrow = () => {
 
     const scrollDown = () => {
-        const aboutMeSection = document.getElementById('#aboutMeSection')
-        if (aboutMeSection) aboutMeSection.scrollIntoView();
+        const aboutMeSection = document.getElementById('aboutMeSection')
+        if (aboutMeSection) aboutMeSection.scrollIntoView({ behavior: 'smooth' });
     }
 
     return (
