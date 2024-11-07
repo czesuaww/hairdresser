84a01130f79e59dcb777de862b5a9626dae7d7f3
Grzegorz_Szwed <greg152295@gmail.com>
2024-11-07 18:32:35 +0100

    Added MiniGallery component to appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header.js';
 import About from './components/About/About.js';
-
+import Minigallery from './components/Minigallery/Minigallery.js';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Minigallery />
       <About />
     </div>
   );
